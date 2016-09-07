da48a92a15001fc21fbe3baf128021b53f6c0d13
KrusH <krushik@ukr.net>
2016-09-07 15:36:53 +0300

    splash

---
+++ app/src/main/java/info/krushik/android/phonebook/SplashScreenActivity.java
@@ -0,0 +1,39 @@
+package info.krushik.android.phonebook;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final int SPLASH_TIME = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.move_up);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setAnimation(anim);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, 4000);
+
+    }
+}
